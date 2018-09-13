8d1f60be15eff663a6bd9a39db027b04dfe0ec04
PMH <kok4575@gmail.com>
2018-09-13 22:56:17 +0900

    [09/13] Test.js Add

---
+++ TEST.js
@@ -0,0 +1,24 @@
+/*
+	μBot v4.0 Test. 
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
+// API Require
+const API = require('discord.js'); // Main API
+
+// TTS Require
+const TTS = require("text-to-mp3");
+
+// Login Require
+const token = process.env.token; // Set Bot Token
+const mu = new API.Client(); // Take a Bot From Main API
+
+mu.login(token); // Login! Go, μBot!
+
+mu.on('ready', () => {
+	console.log("Test Completed!")
+});
