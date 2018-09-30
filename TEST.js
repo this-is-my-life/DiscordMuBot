a2cc442b64b70437f82e88f3104f2fa01587796a
PMH <kok4575@gmail.com>
2018-09-30 15:26:48 +0900

    [09/30] CI Notch Add

--- TEST.js
+++ TEST.js
@@ -25,9 +25,14 @@ const token = process.env.token; // Set Bot Token
 console.log("Token Loaded Correctly");
 console.log("Discord API Client Login");
 const mu = new API.Client(); // Take a Bot From Main API
+mu.login(token);
 console.log("Login Correctly")
-
-console.log("Returning Code: 0");
-
-console.log("	μBot v4.0 Test.\n	---------------------\n	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ \nCopyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.\n\n\n");
-return console.log("----------------------------------------");
+let CInotchChannel = "459308509738041357";
+let CIpassEmb = new API.RichEmbed()
+	.setTitle('[DiscordMuBot:μBot-ver4] Travis CI Build Passing!')
+	.setURL("https://travis-ci.org/PMHStudio/DiscordMuBot/")
+	.setAuthor("Travis CI", "https://styleguide.travis-ci.com/favicon.ico")
+	.setDescription("Yeeeeeeesssssssssss!");
+CInotchChannel.send(CIpassEmb);
+console.log("	μBot v4.0 Test.\n	---------------------\n	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ \n	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.\n\n\n");
+console.log("-----------------------------------------------");
