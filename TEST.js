c8088ab980f54815eb96cc4df82b60d965ff0fcd
PMH <kok4575@gmail.com>
2018-09-30 15:45:16 +0900

    [09/30] Hot Fix?

--- TEST.js
+++ TEST.js
@@ -27,12 +27,11 @@ console.log("Discord API Client Login");
 const mu = new API.Client(); // Take a Bot From Main API
 mu.login(token);
 console.log("Login Correctly")
-let CInotchChannel = "459308509738041357";
 let CIpassEmb = new API.RichEmbed()
 	.setTitle('[DiscordMuBot:μBot-ver4] Travis CI Build Passing!')
 	.setURL("https://travis-ci.org/PMHStudio/DiscordMuBot/")
 	.setAuthor("Travis CI", "https://styleguide.travis-ci.com/favicon.ico")
 	.setDescription("Yeeeeeeesssssssssss!");
-CInotchChannel.send(CIpassEmb);
+mu.channels.get("459308509738041357").send(CIpassEmb);
 console.log("	μBot v4.0 Test.\n	---------------------\n	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ \n	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.\n\n\n");
 console.log("-----------------------------------------------");
