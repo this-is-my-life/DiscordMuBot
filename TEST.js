d8f001ae77a5a4795c2801f201c3d3d4ea962206
PMH <kok4575@gmail.com>
2018-10-02 22:04:17 +0900

    [10/02] Travis CI Updated

--- TEST.js
+++ TEST.js
@@ -19,13 +19,33 @@ console.log("TTS API(Google TTS Library) Load...");
 const TTS = require("text-to-mp3");
 console.log("TTS API Loaded Correctly");
 
+// Command Reader Require
+console.log("Command Reader Load...");
+const cmds = require("fs");
+console.log("Reader Loaded Correctly");
+
 // Login Require
 console.log("Token Load...");
 const token = process.env.token; // Set Bot Token
 console.log("Token Loaded Correctly");
 console.log("Discord API Client Login");
 const mu = new API.Client(); // Take a Bot From Main API
-console.log("Login Correctly")
+console.log("Login Correctly");
+console.log("Command Load...");
+mu.commands = new API.Collection()
+cmds.readdir("./muc/", (err, files) => {
+if (err) return console.log("\n\nRead Commands Error!______________________________\n\n" + err);
+let jsfile = files.filter(f => f.split(".").pop() === "js")
+if(jsfile.length <= 0){
+console.log("Error(E404): Couldn't find commands.");
+return;
+}
+jsfile.forEach((f, i) =>{
+let props = require(`./muc/${f}`);
+console.log(`CommandLoad: Ready(${f})`);
+mu.commands.set(props.help.name, props);
+});
+});
 
 console.log("Returning Code: 0");
 
