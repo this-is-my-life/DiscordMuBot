97d218829f5decfb185939396eac4e9497daf180
PMH <kok4575@gmail.com>
2018-10-12 22:40:37 +0900

    [10/12] Debug Line Delete

--- CORE.js
+++ CORE.js
@@ -74,7 +74,6 @@ console.log("\n\n\nμBot v5.0 Core Session is Start!\n------------------Bot Star
 
 // Bot Readying__________________________________
 	mu.on("ready", async () => {
-		mu.users.get("411381903707471895").send("mubotapi> Heroku Online Checked...")
 		console.log("-----------------------------------------------------------\n\n	μBot is Running Correctly! | " + mu.status + " | " + mu.guilds.size + " Servers | " + mu.channels.size + " Channels | " + mu.users.size + " Users\n\nInput Log:");
 		mu.user.setActivity("Messages | mu!help", {type: "WATCHING"});
 	});
