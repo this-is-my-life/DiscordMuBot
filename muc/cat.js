d7a21c1abc8bdbe336572c7703e1cfad3294eac6
PMH Studio / WSF <44047052+WhiteSnowFlakes@users.noreply.github.com>
2018-11-21 20:37:43 +0900

    [11/21] Mu is Cat!

---
+++ muc/cat.js
@@ -0,0 +1,35 @@
+/*
+    μBot v5.0 Command: Ping. 
+    ---------------------
+    PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+    Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+    
+    * Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+const request = require("request");
+const randomHexColor = require("random-hex-color");
+
+module.exports.run = async (mu, input, pars) => {
+    request.get('http://thecatapi.com/api/images/get?format=src&type=png', {
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+                let eDog = new API.RichEmbed()
+                .setColor(randomHexColor())
+                .setTitle(`Cats, HERE!`)
+                .setURL(response.request.uri.href)
+                .setDescription(`${input.author} said " Cat!!"`)
+                .setImage(response.request.uri.href)
+                .setFooter("Powered by thecatapi.com");
+                input.channel.send(eDog);
+        } else {
+            console.log(error);
+        }
+    })
+}
+
+module.exports.help = {
+    name: "cat"
+}
