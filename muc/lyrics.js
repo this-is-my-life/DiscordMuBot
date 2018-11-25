961c0ef6cfa4031e13c6297cd06cf3d4c5b38395
PMHStudio <kok4575@gmail.com>
2018-11-25 15:24:28 +0900

    [11/25] Lyrics Error Founder

--- muc/lyrics.js
+++ muc/lyrics.js
@@ -14,16 +14,20 @@ const randomHexColor = require("random-hex-color");
 module.exports.run = async (mu, input, pars) => {
     let songs = pars.join(" ").slice(0);
     let { body } = await superagent
-        .get(`https://some-random-api.ml/lyrics?title=${songs}`);
-    let eLyrics = new API.RichEmbed()
-        .setColor(randomHexColor())
-        .setTitle(`Lyrics, HERE!`)
-        .setURL(body.links.genius)
-        .setDescription(`"${body.title}" -${body.author}`)
-        .setThumbnail(`${body.thumbnail.genius}`)
-        .setFooter("Powered by some-random-api.ml");
-    input.channel.send(`${body.lyrics}\n[Lyric End]`);
-    input.channel.send(eLyrics);
+    .get(`https://some-random-api.ml/lyrics?title=${songs}`);
+    if (body.error) {
+        input.channel.send(`body.error`);
+    } else {
+        let eLyrics = new API.RichEmbed()
+            .setColor(randomHexColor())
+            .setTitle(`Lyrics, HERE!`)
+            .setURL(body.links.genius)
+            .setDescription(`"${body.title}" -${body.author}`)
+            .setThumbnail(`${body.thumbnail.genius}`)
+            .setFooter("Powered by some-random-api.ml");
+        input.channel.send(`${body.lyrics}\n[Lyric End]`);
+        input.channel.send(eLyrics);
+    }
 }
 
 module.exports.help = {
