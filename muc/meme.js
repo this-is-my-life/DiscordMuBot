379a3434d5319181ea607aa469fea711cf46e499
PMHStudio <kok4575@gmail.com>
2018-11-25 11:45:00 +0900

    [11/25] Meme Command Added

---
+++ muc/meme.js
@@ -0,0 +1,30 @@
+/*
+	μBot v5.0 Command: Ping. 
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+const randomHexColor = require('random-hex-color');
+const superagent = require("superagent");
+
+module.exports.run = async (mu, input, pars) => {
+    let { body } = await superagent
+    .get(`https://api-to.get-a.life/meme`);
+    let eMeme = new API.RichEmbed()
+    .setColor(randomHexColor())
+    .setTitle(`MEMEs, HERE!`)
+    .setURL(body.url)
+    .setDescription(`${input.author} said\n"${body.text}"`)
+    .setImage(body.url)
+    .setFooter("Powered by api-to.get-a.life");
+    input.channel.send(eMeme);
+}
+
+module.exports.help = {
+    name: "dog",
+    description: "woof"
+}
