2c53d1444e8cb57dc2f6cee5e4776e70d766ba78
PMHStudio <kok4575@gmail.com>
2018-11-26 20:26:56 +0900

    [11/26] Okay It's Noisy

--- CORE.js
+++ CORE.js
@@ -91,16 +91,6 @@ console.log("\n\n\nμBot v7.0 Core Session is Start!\n------------------Bot Star
 // Bot Commanding________________________________
 	mu.on("message", async input => {
 		if (input.guild.id === "264445053596991498") return;
-        if (input.author.bot) {
-            // React Messages
-            input.react('👍');
-            input.react('👎');
-            input.react('👌');
-            input.react('🖕');
-            input.react('💯');
-            input.react('💥');
-            input.react('🤔');
-        }
 
 		// MuteCoin
 		{
