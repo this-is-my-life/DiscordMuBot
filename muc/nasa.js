313349036771055e41766edd8feb10cac9362f0e
PMH <kok4575@gmail.com>
2018-12-02 23:17:06 +0900

    [12/02] Remove Error Checker

--- muc/nasa.js
+++ muc/nasa.js
@@ -13,16 +13,15 @@ const superagent = require("superagent");
 const randomHexColor = require("random-hex-color");
 
 module.exports.run = async (mu, input, pars, prefix , nasa) => {
-    let { body1 } = await superagent
+    let { body } = await superagent
         .get(`https://api.nasa.gov/planetary/apod?api_key=GpELYI28U6YMlWtNjDcF5IOunjRi9ZIFPJcTHDdo`);
-    if (!body1.media_type === "image" && body1.service === "v1") { return input.channel.send("NASA API Error!"); }
     let eNasa = new API.RichEmbed()
         .setColor(randomHexColor())
         .setTitle(`Space, HERE! (Click Here to See HD Image)`)
-        .setURL(body1.hdurl)
+        .setURL(body.hdurl)
         .setDescription(`Astronomy Picture of the Day (오늘의 우주)`)
-        .addField(body1.title, body1.explanation)
-        .setImage(body1.url)
+        .addField(body.title, body.explanation)
+        .setImage(body.url)
         .setFooter("Powered by nasa.gov - Astronomy Picture of the Day");
     input.channel.send(eNasa);
 }
