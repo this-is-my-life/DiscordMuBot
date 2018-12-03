64415233a65ced39d43ec4a18ec86dd088f42653
WhiteSnowFlakes <lhj082926@gmail.com>
2018-12-03 13:05:24 +0900

    [12/03] ADD Warn!

---
+++ muc/경고.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (mu, input, pars) => {
+
+  //!warn @daeshan <reason>
+  if(!input.member.hasPermission("MANAGE_MEMBERS")) return input.reply("No can do pal!");
+  let wUser = input.guild.member(input.mentions.users.first()) || input.guild.members.get(pars[0])
+  if(!wUser) return input.reply("Couldn't find them yo");
+  if(wUser.hasPermission("MANAGE_MESSAGES")) return input.reply("They waaaay too kewl");
+  let reason = pars.join(" ").slice(22);
+
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
+
+  warns[wUser.id].warns++;
+
+  fs.writeFile("./Saved/warn.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
+  });
+
+  let warnEmbed = new Discord.RichEmbed()
+  .setDescription("Warns")
+  .setAuthor(input.author.username)
+  .setColor("#fc6400")
+  .addField("Warned User", `<@${wUser.id}>`)
+  .addField("Warned In", input.channel)
+  .addField("Number of Warnings", warns[wUser.id].warns)
+  .addField("Reason", reason);
+
+  let warnchannel = input.guild.channels.find(`name`, "incidents");
+  if(!warnchannel) return input.reply("Couldn't find channel");
+
+  warnchannel.send(warnEmbed);
+
+  if(warns[wUser.id].warns == 3){
+    let muterole = input.guild.roles.find(`name`, "muted");
+    if(!muterole) return input.reply("You should create that role dude.");
+
+    let mutetime = "3h";
+    await(wUser.addRole(muterole.id));
+    input.channel.send(`<@${wUser.id}> has been temporarily muted`);
+
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      input.reply(`<@${wUser.id}> has been unmuted.`)
+    }, ms(mutetime))
+  }
+  if(warns[wUser.id].warns == 5){
+    input.guild.member(wUser).ban(reason);
+    input.reply(`<@${wUser.id}> has been banned.`)
+  }
+
+}
+
+module.exports.help = {
+  name: "warn"
+}
