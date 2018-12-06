3d2c400fc73eeb67fc69c1c2d0ff3429d45bc56e
PMH <kok4575@gmail.com>
2018-12-06 16:31:35 +0900

    [12/06] Hot Fixed

--- muc/계산기.js
+++ muc/계산기.js
@@ -12,14 +12,14 @@ const randomHexColor = require('random-hex-color');
 
 module.exports.run = async (mu, input, pars) => {
 	const filter = m => m.author.id === input.author.id
-	input.channel.send("입력 받을 값의 진수를 입력하세요 (2, 10, 16 중에서 숫자만 입력)\n\"취소\"를 입력하거나 30초를 쓸때없이 기다리시면 뮤봇이 빡쳐서 취소할껍니다").then(this, err => this.delete(30000));
+	input.channel.send("입력 받을 값의 진수를 입력하세요 (2진수, 10진수, 16진수 지원)\n\"취소\"를 입력하거나 30초를 쓸때없이 기다리시면 뮤봇이 빡쳐서 취소할껍니다").then(this, err => this.delete(30000));
 	input.channel.awaitMessages(filter, {
 		max: 1,
 		time: 30000
 	}).then(collected => {
 		const col1input = collected.content;
 		if (col1input === "취소") { input.channel.send("취소되었습니다").then(this, err => this.delete(2000)); } else {
-			input.channel.send("출력할 값의 진수를 입력하세요 (2, 10, 16 중에서 숫자만 입력)\n\"취소\"를 입력하거나 30초를 쓸때없이 기다리시면 뮤봇이 빡쳐서 취소할껍니다").then(this, err => this.delete(30000));
+			input.channel.send("출력할 값의 진수를 입력하세요 (2진수, 10진수, 16진수 지원)\n\"취소\"를 입력하거나 30초를 쓸때없이 기다리시면 뮤봇이 빡쳐서 취소할껍니다").then(this, err => this.delete(30000));
 			input.channel.awaitMessages(filter, {
 				max: 1,
 				time: 30000
@@ -32,36 +32,38 @@ module.exports.run = async (mu, input, pars) => {
 						time: 30000
 					}).then(collected => {
 						const colinput = collected.content;
-						const colresult;
-						if (col1input === "2") {
-							if (col2input === "2"){
+						let colresult;
+						if (col1input === "2" || col1input === "2진수" || col1input === "이진수" || col1input === "바이너리") {
+							if (col2input === "2" || col2input === "2진수" || col2input === "이진수" || col2input === "바이너리"){
 								colresult = colinput;
-							} else if (col2input === "10"){
+							} else if (col2input === "10" || col2input === "10진수" || col2input === "십진수"){
 								colresult = parseInt(colinput, 2);
-							} else if (col2input === "16"){
+							} else if (col2input === "16" || col2input === "16진수" || col2input === "십육진수" || col2input === "핵사"){
 	
+							} else {
+								colresult = "Error"
 							}
 	
-						} else if (col1input === "10") {
-							if (col2input === "2"){
+						} else if (col1input === "10" || col1input === "10진수" || col1input === "십진수") {
+							if (col2input === "2" || col2input === "2진수" || col2input === "이진수" || col2input === "바이너리"){
 								if (Number(colinput).toString(2).substring(54)) {
 									colresult = "overflowed";
 								} else {
 									colresult = Number(colinput).toString(2).substring(0, 54);
 								}
 	
-							} else if (col2input === "10"){
+							} else if (col2input === "10" || col2input === "10진수" || col2input === "십진수"){
 								colresult = colinput;
-							} else if (col2input === "16"){
+							} else if (col2input === "16" || col2input === "16진수" || col2input === "십육진수" || col2input === "핵사"){
 	
 							}
 	
-						} else if (col1input === "16") {
-							if (col2input === "2"){
+						} else if (col1input === "16" || col1input === "16진수" || col1input === "십육진수" || col1input === "핵사") {
+							if (col2input === "2" || col2input === "2진수" || col2input === "이진수" || col2input === "바이너리"){
 	
-							} else if (col2input === "10"){
+							} else if (col2input === "10" || col2input === "10진수" || col2input === "십진수"){
 	
-							} else if (col2input === "16"){
+							} else if (col2input === "16" || col2input === "16진수" || col2input === "십육진수" || col2input === "핵사"){
 								colresult = colinput;
 							}
 						}
