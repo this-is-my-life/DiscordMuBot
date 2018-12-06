80d3e6f70254322024946db0a2971fe1f1f92345
PMH <kok4575@gmail.com>
2018-12-06 18:42:11 +0900

    [12/06] Dummy Data Added

---
+++ muc/토스트.js
@@ -0,0 +1,19 @@
+/*
+	μBot v5.0 Command: Ping. 
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+
+module.exports.run = async (mu, input, pars) => {
+
+}
+
+module.exports.help = {
+	name: "빵굽기",
+	description: "투자"
+}
