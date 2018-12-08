89e100428c905b74aa8946efa035d44b9b48854d
PMH Studio / WSF <44047052+WhiteSnowFlakes@users.noreply.github.com>
2018-12-08 11:37:38 +0900

    [12/08] RollBack

--- muc/도박.js
+++ muc/도박.js
@@ -8,66 +8,66 @@ module.exports.run = async (mu, input, pars) => {
     let Slot1; // 첫번째 슬롯 값의 대한 메세지 스트링
     let Slot2; // 두번째 슬롯 값의 대한 메세지 스트링
     let Slot3; // 세번째 슬롯 값의 대한 메세지 스트링
-    let SlotMax = 100; // 생성될 슬롯 값의 최대값
+    let SlotMax = 7; // 생성될 슬롯 값의 최대값
     let SlotResult; // 슬롯 123 값을 비교한 결과의 대한 메세지 스트링
     let Random_Number1 = Math.floor(Math.random() * (SlotMax)) + 0; // 첫번째 슬롯 값 추출
     let Random_Number2 = Math.floor(Math.random() * (SlotMax)) + 0; // 두번째 슬롯 값 추출
     let Random_Number3 = Math.floor(Math.random() * (SlotMax)) + 0; // 세번째 슬롯 값 추출
    
     // 첫번째 슬롯 값의 대한 메세지 스트링
-    if (Random_Number1 << 30) {
+    if (Random_Number1 == 0) {
         Slot1 = ":zero:";
-    } else if (Random_Number1 << 40) {
+    } else if (Random_Number1 == 1) {
         Slot1 = ":one:";
-    } else if (Random_Number1 << 50) {
+    } else if (Random_Number1 == 2) {
         Slot1 = ":two:";
-    } else if (Random_Number1 << 60) {
+    } else if (Random_Number1 == 3) {
         Slot1 = ":three:";
-    } else if (Random_Number1 << 70) {
+    } else if (Random_Number1 == 4) {
         Slot1 = ":four:";
-    } else if (Random_Number1 << 80) {
+    } else if (Random_Number1 == 5) {
         Slot1 = ":five:";
-    } else if (Random_Number1 << 100) {
+    } else if (Random_Number1 == 6) {
         Slot1 = ":six:";
-    } else if (Random_Number1 << 101) {
+    } else if (Random_Number1 == 7) {
         Slot1 = ":seven:";
     }
 
     // 두번째 슬롯 값의 대한 메세지 스트링
-    if (Random_Number2 << 20) {
+    if (Random_Number2 == 0) {
         Slot2 = ":zero:";
-    } else if (Random_Number2 << 30) {
+    } else if (Random_Number2 == 1) {
         Slot2 = ":one:";
-    } else if (Random_Number2 << 40) {
+    } else if (Random_Number2 == 2) {
         Slot2 = ":two:";
-    } else if (Random_Number2 << 50) {
+    } else if (Random_Number2 == 3) {
         Slot2 = ":three:";
-    } else if (Random_Number2 << 70) {
+    } else if (Random_Number2 == 4) {
         Slot2 = ":four:";
-    } else if (Random_Number2 << 80) {
+    } else if (Random_Number2 == 5) {
         Slot2 = ":five:";
-    } else if (Random_Number2 << 90) {
+    } else if (Random_Number2 == 6) {
         Slot2 = ":six:";
-    } else if (Random_Number2 << 101) {
+    } else if (Random_Number2 == 7) {
         Slot2 = ":seven:";
     }
     
     // 세번째 슬롯 값의 대한 메세지 스트링
-    if (Random_Number3 << 10) {
+    if (Random_Number3 == 0) {
         Slot3 = ":zero:";
-    } else if (Random_Number3 << 40) {
+    } else if (Random_Number3 == 1) {
         Slot3 = ":one:";
-    } else if (Random_Number3 << 60) {
+    } else if (Random_Number3 == 2) {
         Slot3 = ":two:";
-    } else if (Random_Number3 << 76) {
+    } else if (Random_Number3 == 3) {
         Slot3 = ":three:";
-    } else if (Random_Number3 << 78) {
+    } else if (Random_Number3 == 4) {
         Slot3 = ":four:";
-    } else if (Random_Number3 << 90) {
+    } else if (Random_Number3 == 5) {
         Slot3 = ":five:";
-    } else if (Random_Number3 << 95) {
+    } else if (Random_Number3 == 6) {
         Slot3 = ":six:";
-    } else if (Random_Number3 << 101) {
+    } else if (Random_Number3 == 7) {
         Slot3 = ":seven:";
     }
     
