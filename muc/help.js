60dc31431febe8c6afdb35ccc51e9be6e0759d67
PMH Studio / WSF <44047052+WhiteSnowFlakes@users.noreply.github.com>
2018-12-10 20:25:41 +0900

    [12/10] "μBot v7.0 Command" Add to Muc!

--- muc/help.js
+++ muc/help.js
@@ -1,5 +1,5 @@
 /*
-	μBot v5.0 Command: Command Book. 
+	μBot v7.0 Command.
 	---------------------
 	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
 	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
--- muc/도박.js
+++ muc/도박.js
@@ -1,3 +1,12 @@
+/*
+	μBot v7.0 Command.
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
 const API = require("discord.js");
 const mute = require("../Saved/UsersCoin.json");
 const cmds = require("fs");
