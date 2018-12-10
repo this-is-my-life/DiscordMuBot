6df582b36542179edbde77b5c889ba70f3b5a852
PMH Studio / WSF <44047052+WhiteSnowFlakes@users.noreply.github.com>
2018-12-10 19:54:49 +0900

    [12/10] Build Info Update!

--- muc/buildinfo.js
+++ muc/buildinfo.js
@@ -15,10 +15,10 @@ module.exports.run = async (mu, input, pars) => {
     .set("Accept", "application/json;version=3").set("Authorization", `Bearer ${process.env.muhu}`).then(res => {
         let herokuEmb = new API.RichEmbed()
         .setColor("#6762a6")
-        .setTitle("히로쿠 뮤봇 빌드 인포")
+        .setTitle("Heroku `μBot 빌드정보`")
         .setTimestamp()
         .setThumbnail("http://www.stickpng.com/assets/images/58480873cef1014c0b5e48ea.png")
-        .setDescription(`현재 작동되고 있는 뮤봇은 정식, 승인된 디벨로퍼인\n${res.body.slice(-1)[0].user.email}으로부터 ${res.body.slice(-1)[0].updated_at}에 빌드된\n${res.body.slice(-1)[0].source_blob.version}버전을 사용하여\n빌드에 ${res.body.slice(-1)[0].status}된 버전을 사용중입니다`);
+        .setDescription(`현재 사용중인 μBot은 정식 승인된 개발자인\n${res.body.slice(-1)[0].user.email}으로부터 \n${res.body.slice(-1)[0].updated_at}에 빌드된\n${res.body.slice(-1)[0].source_blob.version}버전을 사용하여\n빌드에 ${res.body.slice(-1)[0].status}된 버전을 사용중입니다`);
         input.channel.send(herokuEmb);
     }).catch(err => console.log(err));
 }
