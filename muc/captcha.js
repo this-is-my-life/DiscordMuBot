bc776a467875901a7bcbe41d9f5b380ea0f2fab3
PMH <kok4575@gmail.com>
2018-12-10 23:03:17 +0900

    [12/10] I'm not a robot

---
+++ muc/captcha.js
@@ -0,0 +1,28 @@
+/*
+    μBot v7.0 Command.
+    ---------------------
+    PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+    Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+    
+    * Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+const superagent = require("superagent");
+const randomHexColor = require("random-hex-color");
+
+module.exports.run = async (mu, input, pars) => {
+    let capUser = input.guild.member(input.mentions.users.first()).user || input.guild.members.get(pars[0]).user || input.author;
+    superagent.get(`https://nekobot.xyz/api/imagegen?type=captcha&url=${capUser.displayAvatarURL}&username=${capUser.username}`)
+    .then(res => {
+        let CapEmb = new API.RichEmbed()
+        .setImage(res.body.message)
+        .setColor(randomHexColor());
+        input.channel.send(CapEmb);
+    }).catch(err => { if (err) console.log(err)});
+}
+
+module.exports.help = {
+    name: "켑챠",
+    description: "로봇이아닙니다"
+}
