0762b44f1a75f8739df2af6f961abd761cfa4fa8
PMH <kok4575@gmail.com>
2018-12-19 13:17:13 +0900

    [12/19] Hot Fix

--- muc/mutecoin.js
+++ muc/mutecoin.js
@@ -8,23 +8,21 @@
 */
 
 const API = require("discord.js");
-let mute = require("../Saved/UsersCoin.json");
 const randomHexColor = require("random-hex-color");
+const superagent = require("superagent");
 
 module.exports.run = async (mu, input, pars) => {
-	if (!mute[input.author.id]) {
-		mute[input.author.id] = {
-			mute: 0
-		};
-	}
-	let userCoin = mute[input.author.id].mute;
-    let eCoinEmb = new API.RichEmbed()
-    .setTitle(`Hum... ${input.member.displayName} Have....`)
-    .setColor(randomHexColor())
-    .setDescription(`${userCoin} MUC(MuteCoin)!`);
-    input.channel.send(eCoinEmb);
+	superagent.get(`https://mubotdb.herokuapp.com/api/UsersCoin.json/${input.author.id}`)
+	.then(res => {
+		let eCoinEmb = new API.RichEmbed()
+		.setTitle(`Umm... ${input.member.displayName} has....`)
+		.setColor(randomHexColor())
+		.setDescription(`${res.body.UsersCoin} MUC!`);
+		input.channel.send(eCoinEmb);
+	});
 }
 
 module.exports.help = {
 	name: "coin"
 }
+s
