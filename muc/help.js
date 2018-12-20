86ece38326127ddc8b89e9d1e15fa14f014f68a5
PMH <kok4575@gmail.com>
2018-12-20 21:05:16 +0900

    [12/20] Command Book Updated!

--- muc/help.js
+++ muc/help.js
@@ -11,43 +11,60 @@ const API = require("discord.js");
 
 module.exports.run = async (mu, input, pars, prefix) => {
 	const helpemb = new API.RichEmbed({
-		"title": "Do you wanna view μBot's :regional_indicator_c::o2::regional_indicator_d::regional_indicator_e:?　:arrow_left: Click Here!",
-  		"description": "Copyright (c) 2018. PMH Studio / PMH & WSF. MIT Licenced.\n\n\n",
-		"url": "https://github.com/PMHStudio/DiscordMuBot/tree/%CE%BCBot-ver5",
-  		"color": 7506394,
-  		"timestamp": "2018-06-19T04:33:13.297Z",
-  		"footer": {
-  		  	"icon_url": "https://images-ext-1.discordapp.net/external/Ptv3hyLB2p7wVWbC7iKUMxBoyhW-g2i__26OzuWFvgo/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/458625370334429184/c0e1149749f9c740de6255432c50a70c.png",
-			"text": "Thanks for using Our μBot!"
-  		},
-  		"author": {
-    		"name": "Discord μBot (v.5) Commands!",
-    		"url": "https://discordapp.com",
-    		"icon_url": "http://mubotapi.dothome.co.kr/images/%CE%BCBot%20Kakao%20Profile.jpg"
-  		},
-  		"fields": [
-    		{
-      			"name": "Server(Guild) Manage",
-      			"value": `:information_source:  Server Infomations.\`\`\`fix\n${prefix}serverinfo   (or ${prefix}s)\`\`\`\n\n:speaker: Report Member. (Request Permission: Manage Messages.)\n\`\`\`fix\n${prefix}report <@Mention Member> [Reason]   (or ${prefix}r)\n\`\`\`\n\n:boom:  Kick Member. (Request Permission: Kick Members.)\n\`\`\`fix\n${prefix}kick <@Mention Member> [Reason]   (or ${prefix}k)\`\`\`\n\n:no_entry:  BAN Member. (Request Permission: Ban Members.)\n\`\`\`fix\n${prefix}ban <@Mention Memver> [Reason]   (or ${prefix}b)\`\`\`\n`
-    		},
-    		{
-      			"name": "Voice Channel Commands",
-      			"value": `:triangular_flag_on_post:  Join Voice Channel.\n\`\`\`fix\n${prefix}join   (or ${prefix}j)\`\`\`\n\n:speaking_head:  TTS in Voice Channel.\n\`\`\`fix\n${prefix}tts <Text>   (or ${prefix}t)\n\`\`\`\n\n:hand_splayed:   Leave Voice Channel.\n\`\`\`fix\n${prefix}leave   (or ${prefix}l)\n\`\`\`\n`,
-     		 	"inline": true
-    		},
-    		{
-      			"name": "μBot Info & Credit",
-      			"value": `:raising_hand:  μBot Info & Credit.\n\`\`\`fix\n${prefix}(No Parameter)\n\`\`\`\n\n:question:  μBot Command Book & Bug Report.\n\`\`\`fix\n${prefix}help   (or ${prefix}h)\n\`\`\`\n\n:ping_pong:   Ping!\n\`\`\`fix\n${prefix}ping   (or ${prefix}p)\n\`\`\``,
-      			"inline": true
-    		},
-    		{
-      			"name": "and....?",
-      			"value": `\`\`\`fix\n${prefix}woof\`\`\``,
-      			"inline": true
-			}
-  		]
+		"title": "μBot's Command Book",
+		"description": "**μ's Sites: **[PMH Studio Official Site](http://pmhstudio.co.nf) ● [AI Training Site](http://mubotapi.dothome.co.kr) ● [Github Repo](https://github.com/PMHStudio/DiscordMuBot)\n\n\n**Commands\n────────────**",
+		"color": 7506394,
+		"timestamp": "2018-12-20T09:25:31.239Z",
+		"footer": {
+		  "text": "This Command Book is Completed 30%"
+		},
+		"thumbnail": {
+		  "url": "https://cdn.discordapp.com/attachments/521187311761752087/525245345849147393/Bot_Profile.png"
+		},
+		"author": {
+		  "name": input.author.username,
+		  "icon_url": input.author.displayName
+		},
+		"fields": [
+		  {
+			"name": "Server Management Commands",
+			"value": "User Report __*(Aliase: mu!r)*__ **[Deprecated]**```fix\nmu!report <@Target> [Reason]```User Warn **[New!]**```fix\nmu!warn <@Target> <Reason>```User Kick __*(Aliase: mu!k)*__```fix\nmu!kick <@Target> [Reason]```User Ban __*(Aliase: mu!b)*__```fix\nmu!ban <@Target> [Reason]```",
+			"inline": true
+		  },
+		  {
+			"name": "Voice Channel Commands",
+			"value": "Join Voice Channel __*(Aliase: mu!j)*__```fix\nmu!join```Leave Voice Channel __*(Aliase: mu!l)*__```fix\nmu!leave```TTS in Voice Channel __*(Aliase: mu!t)*__```fix\nmu!tts <Text>```",
+			"inline": true
+		  },
+		  {
+			"name": "Lyrics & Ping Command",
+			"value": "Lyrics Search __*(Aliase: mu!ly)*__```fix\nmu!lyrics <Name>```Ping Pong OㅅO __*(Aliase: mu!p)*__```fix\nmu!ping```",
+			"inline": true
+		  },
+		  {
+			"name": "Infomation Commands",
+			"value": "My User Info __*(Aliase: mu!m)*__```fix\nmu!myinfo```Server Info```fix\nmu!serverinfo```μBot Build Info```fix\nmu!buildinfo```",
+			"inline": true
+		  },
+		  {
+			"name": "And More!",
+			"value": "μBot Info __(Cool Aliase)__```fix\nmu! (Only Prefix)```Github User Info __*(Aliase: mu!gh)*__```fix\nmu!github```NASA Picture of the Day```fix\nmu!nasa```",
+			"inline": true
+		  },
+		  {
+			"name": "Messaging Commands",
+			"value": "Fast React Tool __*(Aliase: mu!rec)*__```fix\nmu!react <@Target>```Say! **[Use Careful]**```fix\nmu!say <Text>```Say And Delete! **[Need Permission]**```fix\nmu!sayd <Text>```",
+			"inline": true
+		  },
+		  {
+			"name": "ImageGen Commands",
+			"value": "DDLC Image Generator```fix\nmu!DDLC```Captcha Image Generator```fix\nmu!cap <@Target>```Fight Image Generator```fix\nmu!win <@1> <@2>```",
+			"inline": true
+		  }
+		]
 	});
-	input.channel.send(helpemb);
+	input.author.send(helpemb);
+	input.channel.send("See Your DM")
 }
 
 module.exports.help = {
