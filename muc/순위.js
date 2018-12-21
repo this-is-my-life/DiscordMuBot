b57be31fbbb8f0dcfae067abb890699f99fd0513
PMH <kok4575@gmail.com>
2018-12-21 19:27:16 +0900

    [12/21] Dummy Data Added

---
+++ muc/순위.js
@@ -0,0 +1,18 @@
+/*
+	μBot v7.0 Command.
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+
+module.exports.run = async (mu, input, pars) => {
+	return;
+}
+
+module.exports.help = {
+    
+}
