3ba97280b7cca1aceb526f17f05dd2bbff3d11db
PMHStudio <kok4575@gmail.com>
2018-12-22 23:39:25 +0900

    [12/22] Null is not Good

--- muc/google.js
+++ muc/google.js
@@ -21,9 +21,7 @@ module.exports.run = async (mu, input, pars) => {
         if (err) console.log(err);
         for (let count = 0; count < res.links.length; count++) {
             let result = res.links[count];
-            if (!result.title) {
-                GoogleEmb.addField(result.href, `[${result.description}](${result.href})`);
-            } else {
+            if (!result.title == null) {
                 GoogleEmb.addField(result.title, `[${result.description}](${result.href})`);
             }
         }
