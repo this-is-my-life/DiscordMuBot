95270f6fa737a1f109fd72b6ac42d8c85807b939
PMH <kok4575@gmail.com>
2018-12-24 12:41:50 +0900

    [12/24] Um..

---
+++ muc/dev.js
@@ -0,0 +1,22 @@
+/*
+	μBot v7.0 Command.
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+const randomHexColor = require("random-hex-color");
+const superagent = require("superagent");
+
+module.exports.run = async (mu, input, pars) => {
+    if (input.author = OAuth2Application.owner) {
+		console.log("Cool!");
+	}
+};
+
+module.exports.help = {
+	name: "뮤트코인"
+};
