2adcdf97e28731959c53400ab499d474184f7329
PMH <kok4575@gmail.com>
2019-01-07 22:42:47 +0900

    [01/07] Dice Embed Edit System Test

---
+++ muc/dice.js
@@ -0,0 +1,59 @@
+/*
+	μBot v7.0 Command.
+	---------------------
+	PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+	Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+	
+	* Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+const superagent = require("superagent");
+const randomHexColor = require("random-hex-color");
+
+module.exports.run = async (mu, input, pars) => {
+    let Emb1 = new API.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/530043751901429762/531827699585515530/1.png")
+    .setColor(randomHexColor());
+    let Emb2 = new API.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/530043751901429762/531827700726235140/2.png")
+    .setColor(randomHexColor());
+    let Emb3 = new API.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/530043751901429762/531827702785638411/3.png")
+    .setColor(randomHexColor());
+    let Emb4 = new API.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/530043751901429762/531827703104667669/4.png")
+    .setColor(randomHexColor());
+    let Emb5 = new API.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/530043751901429762/531827704904024084/5.png")
+    .setColor(randomHexColor());
+    let Emb6 = new API.RichEmbed()
+    .setImage("https://cdn.discordapp.com/attachments/530043751901429762/531827706107658241/6.png")
+    .setColor(randomHexColor());
+    input.channel.send(Emb1).then((q) => {
+        setTimeout(() => {
+            input.channel.send(Emb2).then((q) => {
+                setTimeout(() => {
+                    input.channel.send(Emb3).then((q) => {
+                        setTimeout(() => {
+                            input.channel.send(Emb4).then((q) => {
+                                setTimeout(() => {
+                                    input.channel.send(Emb5).then((q) => {
+                                        setTimeout(() => {
+                                            input.channel.send(Emb6);
+                                        }, 100);
+                                    })
+                                }, 100);
+                            })
+                        }, 100);
+                    })
+                }, 100);
+            })
+        }, 100);
+    })
+};
+
+module.exports.help = {
+    name: "주사위",
+    description: "다이스"
+};
