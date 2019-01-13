174c84380a091d9971fb8c4be32d3100c43a7b02
PMH <kok4575@gmail.com>
2019-01-13 13:24:12 +0900

    [01/13] URL Shorting Command Added

---
+++ muc/short.js
@@ -0,0 +1,30 @@
+/*
+    μBot v7.0 Command.
+    ---------------------
+    PMH Studio / Proj- μBot | Smart & Cute Discord Bot_Mu~☆ 
+    Copyright (c) 2018. PMH Studio / PMH. (kok4575@gmail.com) MIT Licensed.
+    
+    * Requests Node.js & Discord.js
+*/
+
+const API = require("discord.js");
+const superagent = require("superagent");
+const randomHexColor = require("random-hex-color");
+
+module.exports.run = async (mu, input, pars) => {
+    let say = pars.join(" ").slice(0);
+    superagent.post("https://api-ssl.bitly.com/v4/bitlinks")
+    .send(`{"long_url" : "${say}"}`)
+    .then((res) => {
+        let shortEmb = new API.RichEmbed()
+        .setTitle(res.body.link)
+        .setColor(randomHexColor())
+        .setFooter("Powered by bitly.com");
+        input.channel.send(shortEmb);
+    }).catch((err) => { if (err) { console.log(err); }});
+};
+
+module.exports.help = {
+    name: "단축",
+    description: "url"
+};
