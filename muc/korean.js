9d753d2be8ea8e4740db9b156948bf8ad491db08
PMHStudio <pmhstudio.pmh@gmail.com>
2019-05-01 22:51:55 +0900

    [05/01] No Fill Checker Added

--- muc/korean.js
+++ muc/korean.js
@@ -13,20 +13,24 @@ const superagent = require('superagent')
 
 module.exports.run = async (mu, input, pars) => {
   let say = pars.join(' ').slice(0)
-  superagent.get('https://stdict.korean.go.kr/api/search.do?key=' + process.env.dicToken + '&q=' + encodeURI(say))
-    .then((res) => {
-      if (res.text.includes('<item>')) {
-        let dicEmb = new API.RichEmbed()
-          .setTitle('오옷, "' + say + '"라는 단어를 찾았다뮤!')
-          .setColor(randomHexColor())
-          .addField('국립국어원의 표준국어대사전에 따르면...', res.text.split('<![CDATA[')[2].split(']]>')[0])
-        input.channel.send(dicEmb)
-      } else {
-        input.channel.send('응 그딴거 없어~')
-      }
-    }).catch((err) => {
-      console.log(err)
-    })
+  if (!say) {
+    input.channel.send('nu!사전 <검색어>')
+  } else {
+    superagent.get('https://stdict.korean.go.kr/api/search.do?key=' + process.env.dicToken + '&q=' + encodeURI(say))
+      .then((res) => {
+        if (res.text.includes('<item>')) {
+          let dicEmb = new API.RichEmbed()
+            .setTitle('오옷, "' + say + '"라는 단어를 찾았다뮤!')
+            .setColor(randomHexColor())
+            .addField('국립국어원의 표준국어대사전에 따르면...', res.text.split('<![CDATA[')[2].split(']]>')[0])
+          input.channel.send(dicEmb)
+        } else {
+          input.channel.send('응 그딴거 없어~')
+        }
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
 }
 
 module.exports.help = {
